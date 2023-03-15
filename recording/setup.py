4316d0f9055029d9c7350efe5540138d368c39ae
Daniel Calviño Sánchez <danxuliu@gmail.com>
2023-03-15 15:10:46 +0100

    Set parameters in setup.py not properly read from pyproject.toml
    Signed-off-by: Daniel Calviño Sánchez <danxuliu@gmail.com>

--- recording/setup.py
+++ recording/setup.py
@@ -4,4 +4,10 @@
 from setuptools import setup
 
 setup(
+    # pyproject.toml uses different keywords that are not properly converted to
+    # the old ones, so they need to be explicitly set here to be used by stdeb.
+    # "author" and "author_email" can not be set, though, as due to how they are
+    # internally handled by stdeb it ends mixing the author set here with the
+    # author and email set in pyproject.toml.
+    url = "https://github.com/nextcloud/spreed",
 )
